e4fa7d2a333985d90da3f37277a9eda8b27a65ff
Yi-Cheng Peng <alexpeng0401@gmail.com>
2022-08-21 02:10:32 +1000

    feat:seem right

--- Cryptography/Decryptor.py
+++ Cryptography/Decryptor.py
@@ -29,28 +29,38 @@ def calculator(Plaintext, Ciphertext, position):
         cur_posit = position + count * 6
 
         # Find out all the Ys
-        B.append(char_map.get(Ciphertext[cur_posit]))
+        B_sub = []
+        for ele in Ciphertext[cur_posit - position: cur_posit - position + 6]:
+            B_sub.append(char_map.get(ele))
+        dummy = sum(B_sub) % 29
+        B_sub.append(dummy)
+        B.append(B_sub)
 
         # Find out all the Xs
         A_sub = []
         for ele in Plaintext[cur_posit - position: cur_posit - position + 6]:
             A_sub.append(char_map.get(ele))
+        A_sub.append(1)
         A.append(A_sub)
         count += 1
+    
+    A.append([1,1,1,1,1,1,0])
+    B.append([0,0,0,0,0,0,0])
+
     # Calculate K
     A = np.array(A)
-    print(A)
-
-
+    print("original Plaintext \n",A.shape)
     B = np.array(B)
-    print(B)
+    print("\noriginal Ciphertext \n",B)
 
-    K_sub = np.linalg.lstsq(A, B, rcond=None)
+    inv_A = np.linalg.inv(A)
+    print("\n", inv_A.shape)
 
-    return np.array(K_sub)
-K = []
+    
 
-for idx in range(1):
-    K.append(calculator(Plaintext, Ciphertext, idx))
-res = np.mod(K, 29)
-print(res.shape)
+    K_sub = np.matmul(inv_A, B)
+    res = np.mod(K_sub, 29)
+    return res
+
+K = calculator(Plaintext, Ciphertext, 0)
+print(K)
